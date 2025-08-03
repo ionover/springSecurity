6d8f61ba1003de6e0467194499560a2165ffff20
v.ionov <v.ionov@mycrg.ru>
2025-08-03 15:48:57 +0300

    init

--- src/main/java/org/example/spring_security/WebSecurityConfigurerAdapterImpl.java
+++ src/main/java/org/example/spring_security/WebSecurityConfigurerAdapterImpl.java
@@ -1,15 +1,17 @@
 package org.example.spring_security;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
+@Configuration
+@EnableWebSecurity
 public class WebSecurityConfigurerAdapterImpl {
 
     @Bean
@@ -44,10 +46,5 @@ public class WebSecurityConfigurerAdapterImpl {
                                 .build();
 
         return new InMemoryUserDetailsManager(user, admin);
-    }
-
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
     }
 }
