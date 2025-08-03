bd68809eb7bb814e84a1eecb8ad4128db5c2a0a6
v.ionov <v.ionov@mycrg.ru>
2025-08-03 15:35:34 +0300

    init

--- src/main/java/org/example/spring_security/WebSecurityConfigurerAdapterImpl.java
+++ src/main/java/org/example/spring_security/WebSecurityConfigurerAdapterImpl.java
@@ -1,5 +1,21 @@
 package org.example.spring_security;
 
-public class WebSecurityConfigurerAdapterImpl extends WebSecurityConfigurerAdapter {
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
+public class WebSecurityConfigurerAdapterImpl {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        // конфигурация безопасности
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
